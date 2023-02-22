4e53891bfe4fa2b39fc32270600452bf74fa5c2b
catavera <ctaveras1999@gmail.com>
2023-02-22 12:53:52 -0600

    Working, though slow, implementation of augmented dijkstra algorithm for path planning. In need of much refactoring.

---
+++ simplicial_complexes.py
@@ -0,0 +1,283 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+from scipy.spatial import Delaunay
+
+def simp_from_pts(pts):
+    tri = Delaunay(pts).simplices
+    faces = [x for x in tri if len(set(x)) == 3] # face has 3 distinct nodes
+    faces = [tuple(sorted(x)) for x in faces]    # sort node order 
+
+    edge_idx_dict, edge_dict = {}, {}
+    num_edge = 0
+
+    for face in faces:
+        n1, n2, n3 = face
+        face_edges = [(n1, n2), (n1, n3), (n2, n3)]
+
+        for edge in face_edges:
+            if edge not in edge_dict:
+                edge_dict[edge] = num_edge
+                edge_idx_dict[num_edge] = edge
+                num_edge += 1
+
+    edges = [edge_idx_dict[i] for i in range(num_edge)]
+
+    edges.sort(key=lambda x:x[0])
+    faces.sort(key=lambda x:x[0])
+
+    return edges, faces
+
+
+class SimplicialComplex:
+    def __init__(self, pts):
+        self.nodes = pts
+        self.edges, self.faces = simp_from_pts(pts)
+        num_nodes, num_edges, num_faces = len(pts), len(self.edges), len(self.faces)
+
+        # initialize node, edge, and face vectors
+        self.simplex_vectors_init(self, num_nodes, num_edges, num_faces)
+
+        # initialize incidence matrices (assuming all simplices active)
+        self.incidence_matrices_init()
+
+    # initialize node_vec, edge_vec, and face_vec
+    def simplex_vectors_init(self, num_nodes, num_edges, num_faces):
+        num_nodes, num_edges, num_faces = len(self.nodes), len(self.edges), len(self.faces)
+        self.num_nodes, self.num_edges, self.num_faces = num_nodes, num_edges, num_faces
+        self.node_vec = np.zeros(num_nodes, dtype=int)
+        self.edge_vec = np.zeros(num_edges, dtype=int)
+        self.face_vec = np.zeros(num_faces, dtype=int)
+
+    # initialize incidence matrices (assuming all simplices active)
+    def incidence_matrices_init(self):
+        B0 = np.zeros((self.num_nodes, self.num_edges))
+        B1 = np.zeros((self.num_edges, self.num_faces))
+
+        for i, edge in enumerate(self.edges):
+            v1, v2 = edge
+            B0[v1, i] = -1
+            B0[v2, i] = 1
+        
+        for i, face in enumerate(self.faces):
+            v1, v2, v3 = face
+            e1, e2, e3 = (v1, v2), (v1, v3), (v2, v3)
+            e1_idx, e2_idx, e3_idx = self.edges.index(e1), self.edges.index(e2), self.edges.index(e3)
+            B1[e1_idx, i] = 1
+            B1[e2_idx, i] = -1
+            B1[e3_idx, i] = 1
+
+        self.B0_main, self.B1_main = B0, B1
+
+    def add_simplices(self, simp_set):
+        """
+        simplices: dictionary of simplices to be added.
+        dictionary keys can be 0, 1, and/or 2
+        e.g. simplicies[0] = [1, 3, 4] 
+        (set containing 1st, 3rd and 4th node in mesh) 
+
+        Want to add simplices in such a way that simplicial complex structure is 
+        maintained. Therefore, if we add a face, we must ensure to add its 
+        edges and nodes if they aren't already in the complex.
+        """
+        simp_dict = {0:set(), 1:set(), 2:set()}
+        
+        for i in range(3):
+            if i in simp_set:
+                simp_dict[i] = simp_dict[i].union(simp_set[i])
+
+        # Add faces
+        for face in simp_dict[2]:
+            n1, n2, n3 = self.faces[face]
+            self.face_vec[face] = 1
+
+            e1, e2, e3 = (n1, n2), (n1, n3), (n2, n3)
+
+            e1_idx, e2_idx, e3_idx = self.edges.index(e1), self.edges.index(e2), self.edges.index(e3)
+
+            nodes_needed, edges_needed = set([n1, n2, n3]), set([e1_idx, e2_idx, e3_idx])
+
+            simp_dict[0] = simp_dict[0].union(nodes_needed)
+            simp_dict[1] = simp_dict[1].union(edges_needed)
+
+        # Add edges
+        for edge in simp_dict[1]:
+            self.edge_vec[edge] = 1
+            n1, n2 = self.edges[edge]
+
+            nodes_needed = set([n1, n2])
+            simp_dict[0] = simp_dict[0].union(nodes_needed)
+            
+        # Add nodes
+        for node in simp_dict[0]:
+            self.node_vec[node] = 1
+
+        self.make_graph()
+
+    def remove_simplices(self, simp_set):
+        simp_dict = {0:set(), 1:set(), 2:set()}
+        
+        for i in range(3):
+            if i in simp_set:
+                simp_dict[i] = simp_dict[i].union(simp_set[i])
+
+        for node in simp_dict[0]:
+            self.node_vec[node] = 0
+
+            for i, edge in enumerate(self.edges):
+                if node in edge:
+                    simp_dict[1] = simp_dict[1].union(set([i]))
+                
+            for i, face in enumerate(self.faces):
+                if node in face:
+                    simp_dict[2] = simp_dict[2].union(set([i]))
+
+        for edge in simp_dict[1]:
+            self.edge_vec[edge] = 0
+            n1, n2 = self.edges[edge]
+            for i, face in enumerate(self.faces):
+                if (n1 in face) and (n2 in face):
+                    simp_dict[2] = simp_dict[2].union(set([i]))
+    
+        for face in simp_dict[2]:
+            self.face_vec[face] = 0
+        
+        self.make_graph()
+        
+    def get_incidence_matrices(self):
+        B0 = self.B0_main[self.node_vec.astype(bool),:]
+        B0 = B0[:,self.edge_vec.astype(bool)]
+        B1 = self.B1_main[self.edge_vec.astype(bool),:]
+        B1 = B1[:,self.face_vec.astype(bool)]
+        self.B0, self.B1 = B0, B1
+        return B0, B1
+
+    def make_graph(self, weighted):
+        G = nx.Graph()
+
+        for i, edge in enumerate(self.edges):
+            if self.edge_vec[i]:
+                u, v = edge
+                weight = np.linalg.norm(self.nodes[u] - self.nodes[v]) if weighted else 1
+                G.add_edge(u, v, weight=weight)
+        
+        self.graph = G
+
+    def num_simplices(self, k):
+        if k == 0:
+            res = np.sum(self.node_vec)
+        elif k == 1:
+            res = np.sum(self.edge_vec)
+        else:
+            res = np.sum(self.face_vec)
+        return int(res)
+
+    def star(self, node_vec, edge_vec):        
+        star_edges = edge_vec + np.abs(self.B0_main).T @ node_vec
+        star_edges = (star_edges > 0).astype(int)
+        star_faces = np.abs(self.B1_main).T @ star_edges
+        star_faces = (star_faces > 0).astype(int)
+        return star_edges, star_faces
+        
+    def closure(self, node_vec, edge_vec, face_vec):
+        edges_needed = edge_vec + np.abs(self.B1_main) @ face_vec
+        edges_needed = np.array([int(edges_needed[i] > 0) for i in range(edge_vec.shape[0])])
+        nodes_needed = node_vec + np.abs(self.B0_main) @ edges_needed 
+        nodes_needed = np.array([int(nodes_needed[i] > 0) for i in range(node_vec.shape[0])])
+        return nodes_needed, edges_needed
+
+    def link(self, node_vec, edge_vec, face_vec):
+        # closure (cl) then star (st)
+        nodes_cs, edges_cs = self.closure(node_vec, edge_vec, face_vec)
+        edges_cs, faces_cs = self.star(nodes_cs, edges_cs)
+
+        # star (st) then closure (cl)
+        edges_sc, faces_sc = self.star(node_vec, edge_vec)
+        nodes_sc, edges_sc = self.closure(node_vec, edges_sc, faces_sc)
+
+        # Link = cl(st(x)) \ st(cl(x)) 
+        link_nodes = np.maximum(nodes_sc - nodes_cs, 0)
+        link_nodes = (link_nodes > 0).astype(int)
+
+        link_edges = np.maximum(edges_sc - edges_cs, 0)
+        link_edges = (link_edges > 0).astype(int)
+
+        return link_nodes, link_edges
+
+    def euler_characterstic(self):
+        num_nodes = np.sum(self.node_vec)
+        num_edges = np.sum(self.edge_vec)
+        num_faces = np.sum(self.face_vec)
+        return num_nodes - num_edges + num_faces
+
+    def plot(self):
+        for i, face in enumerate(self.faces):
+            if self.face_vec[i] == 0:
+                continue
+            n1, n2, n3 = face
+            tri = np.vstack([self.nodes[n1], self.nodes[n2], self.nodes[n3]])
+            ti = plt.Polygon(tri, color='white')
+            plt.gca().add_patch(ti)
+
+        for i, edge in enumerate(self.edges):
+            if self.edge_vec[i] == 0:
+                continue
+            n1, n2 = edge
+            line = np.vstack([self.nodes[n1], self.nodes[n2]])
+
+            plt.plot(line[:,0], line[:,1], color='black', linewidth=1, alpha=0.5)
+
+        active_nodes = self.node_vec.astype(bool)
+
+        plt.scatter(self.nodes[active_nodes,0], self.nodes[active_nodes,1], color='blue', alpha=0.5)
+
+    def plot_trajectories(self, trajectories):
+
+        self.plot()
+        
+        colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+        for i, trajectory in enumerate(trajectories):
+            c = colors[i%len(colors)]
+            for edge in trajectory:
+                n1, n2 = edge
+                line = np.vstack([self.nodes[n1], self.nodes[n2]])
+
+                plt.plot(line[:,0], line[:,1], color=c, linewidth=3)
+
+            start_end_nodes = np.array([trajectory[0][0], trajectory[-1][-1]])
+
+            plt.scatter(self.nodes[start_end_nodes, 0], self.nodes[start_end_nodes, 1], color='k', marker='x', linewidths=5)
+
+    def make_holes(self, hole_locs, r=0.25):
+        hole_nodes = set()
+        
+        for i, hole in enumerate(hole_locs):
+            hole_idx = np.linalg.norm(self.nodes - hole, axis=1) < r
+            hole_idx = set(np.where(hole_idx == True)[0])
+            hole_nodes = hole_nodes.union(hole_idx)
+        
+        nodes_to_remove = {0:set(hole_nodes)}
+        self.remove_simplices(nodes_to_remove)
+
+    def harm_proj(self, x, thresh=1e-12):
+        B0, B1 = self.get_incidence_matrices()
+        L = B0.T @ B0 + B1 @ B1.T
+    
+        eig_w, eig_v = np.linalg.eigh(L)
+        ev_list = [(eig_w[i], eig_v[i]) for i in range(len(eig_w))]
+        ev_list.sort(key=lambda var:var[0], reverse=False)
+        eig_w, eig_v = np.array([tup[0] for tup in ev_list]), np.array([tup[1] for tup in ev_list])
+
+        num_holes = np.sum(np.abs(eig_w) < thresh)
+        H = eig_v[:,:num_holes]
+
+        x_hat = x[self.edge_vec.astype(bool)]
+
+        y = (H.T @ np.diag(x_hat)).T
+
+        return y
+
+
+class Plotter:
+    def __init__(self):
+        pass
---
+++ trajectories copy.py
@@ -0,0 +1,232 @@
+import numpy as np
+from copy import deepcopy
+import matplotlib.pyplot as plt
+import networkx as nx
+from simplicial_paths import *
+
+def edges_from_shortest_path(shortest_path):
+    return [(shortest_path[i], shortest_path[i+1]) for i in range(len(shortest_path)-1)]
+            
+
+def get_near_pts(SC, point, num_pts):
+    if num_pts == 1:
+        return [point]
+
+    edges = [s for (i,s) in enumerate(SC.edges) if ((point in s) and (SC.edge_vec[i]))]
+    neighbors = [(j[0] if j[0] != point else j[1]) for j in edges]
+    point_set = [point] + neighbors
+
+    rand_pts = list(np.random.choice(point_set, num_pts))
+
+    pts = rand_pts
+
+    return pts
+
+# Adapted from 
+# https://git.rwth-aachen.de/netsci/trajectory-outlier-detection-flow-embeddings/-/blob/main/util/trajectory.py 
+def generate_paths(SC, start, end, num_trajectories):
+    # Use 0-hop and 1-hop neighborhood of start and end points to generate 
+    # num_trajectories start and end points.
+
+    # Copy graph
+    G = deepcopy(SC.graph)
+
+    # Multiply adjacency matrix by one-hot start point and end point vectors.
+    start_points = get_near_pts(SC, start, num_trajectories)
+    end_points = get_near_pts(SC, end, num_trajectories)
+    
+    paths = []
+
+    for i in range(num_trajectories):
+        node_path = nx.shortest_path(G, start_points[i], end_points[i])
+        path = edges_from_shortest_path(node_path)
+        for (v0, v1) in path:
+            G[v0][v1]['weight'] += 1e-4 
+
+        paths.append(path)
+
+    return paths
+
+def path_interp(SC, path1, path2):
+    new_path, start, end = [], path1[-1][-1], path2[0][0]
+
+    interp_path = generate_paths(SC, start, end, 1)[0]
+    new_path += path1
+    new_path += interp_path
+    new_path += path2
+
+    return [new_path]
+
+
+# return nodes closest to coordinate
+def coord_to_node(SC, coords, num_nodes=1):
+    node_set = []
+    
+    for i, coord in enumerate(coords):
+        node_dists = np.linalg.norm(SC.nodes - coord, axis=1)
+        node_order = np.argsort(node_dists)[:num_nodes]
+        node_set.append(node_order)
+    
+    return np.array(node_set)
+
+# coords is a sequence of nodes that we want our trajectories to pass near.  
+class Trajectory:
+    def __init__(self, SC, coords, num_trajectories, num_nodes):
+        self.SC = SC
+        self.G = deepcopy(SC.graph)
+
+        node_set = coord_to_node(SC, coords, num_nodes)
+        num_coords = len(coords)
+
+        self.all_trajectory_nodes, self.all_trajectories = [], []
+
+        for i in range(num_trajectories):
+            idxs = np.random.choice(np.arange(num_nodes), num_coords)
+            skeleton = [node_set[i, idxs[i]] for i in range(num_coords)]
+            trajectory_nodes = [skeleton[0]]
+            trajectory = []
+    
+            for j in range(len(skeleton)-1):
+                path = nx.shortest_path(self.G, skeleton[j], skeleton[j+1])
+                edges_in_path = edges_from_shortest_path(path)
+                trajectory_nodes += path[1:]
+                trajectory += edges_in_path
+
+                for (v0, v1) in edges_in_path:
+                    self.G[v0][v1]['weight'] += 1e-2
+
+            self.all_trajectory_nodes.append(trajectory_nodes)
+            self.all_trajectories.append(trajectory)
+            
+    def trajectory_to_edgevec(self, trajectory):
+        x_vec = np.zeros_like(self.SC.edge_vec)
+        for _, edge in enumerate(trajectory):
+            e1, e2 = edge
+            parity = -2 * int(e1 > e2) + 1
+            actual_edge = ( min([e1, e2]), max([e1, e2]) )
+            edge_idx = self.SC.edges.index(actual_edge)
+            x_vec[edge_idx] += parity
+
+        return x_vec
+
+    def project_trajectories(self):
+        proj_res_path = []
+        proj_res_val = []
+        for _, trajectory in enumerate(self.all_trajectories):
+            trajectory_projs = []
+            for i, edge in enumerate(trajectory):
+                x_vec = self.trajectory_to_edgevec([edge])
+                edge_proj = self.SC.harm_proj(x_vec)
+                trajectory_projs.append(edge_proj)
+
+            x_vec = self.trajectory_to_edgevec(trajectory)
+            harm_proj_path = self.SC.harm_proj(x_vec)
+            print(harm_proj_path)
+            harm_proj_val = np.sum(harm_proj_path, axis=0)
+            proj_res_path.append(harm_proj_path)
+            proj_res_val.append(harm_proj_val)
+
+        return proj_res_path, np.array(proj_res_val)
+
+    def plot_trajectory_proj(self, class_name, color):
+        proj_res_path, proj_res_val = self.project_trajectories()
+        all_projections = None
+        for _, trajectory_proj in enumerate(proj_res_path):
+            projections = np.cumsum(trajectory_proj, axis=0)
+            if all_projections is None:
+                all_projections = projections
+            else:
+                all_projections = np.vstack([all_projections, projections])
+
+        all_projections = np.array(all_projections)
+        plt.scatter(all_projections[:,0], all_projections[:,1], c=color, marker=',', s=7)
+        plt.scatter(proj_res_val[:,0], proj_res_val[:,1], c=color, label=class_name, marker='*', s=200)
+
+
+"=============================================================================="
+
+if __name__ == "__main__":
+    n_side, point_gen_mode = 20, 0
+    hole_locs = [(0.5, 0.5), (-0.5, -0.5)]
+    r = 0.1
+
+    #np.random.seed(1)
+    pts = generate_pts(point_gen_mode, n_side)
+    SC = SimplicialComplex(pts)
+    all_faces = np.arange(SC.face_vec.shape[0])
+    faces_to_add = {2:set(all_faces)}
+    SC.add_simplices(faces_to_add)
+    SC.make_holes(hole_locs, r)
+
+    num_paths, num_nodes = 2, 6
+
+    trajectories = []
+
+    #coords1 = [(-0.75,-0.75), (0,-0.75), (0,0), (0,0.75), (0.75,0.75)]
+    #trajectories.append(Trajectory(SC, coords1, num_paths, num_nodes))
+
+    #coords2 = [(-0.75,-0.75), (-0.75,0.5), (0.75,0.75)]
+    #trajectories.append(Trajectory(SC, coords2, num_paths, num_nodes))
+
+    #coords3 = [(-0.75,-0.75), (0.5,-0.75), (0.75,0.75)]
+    #trajectories.append(Trajectory(SC, coords3, num_paths, num_nodes))
+
+
+    coords3 = [(-0.75,-0.75), (0.75,-0.75), (0.75,0.75), (-0.75,0.75), (-0.75,-0.75)]
+    trajectories.append(Trajectory(SC, coords3, num_paths, num_nodes))
+
+    proj_res_path = []
+    proj_res_val = []
+    for i, trajectory in enumerate(trajectories):
+        for path in trajectory.path_edges:
+            x_vec = np.zeros_like(SC.edge_vec)
+            parity_sum = 0
+            for i, edge in enumerate(path):
+                e1, e2 = edge
+                parity = -2 * int(e1 > e2) + 1
+                actual_edge = ( min([e1, e2]), max([e1, e2]) )
+                edge_idx = SC.edges.index(actual_edge)
+                x_vec[edge_idx] += parity
+            harm_proj_path = SC.harm_proj(x_vec)
+            harm_proj_val = np.sum(harm_proj_path, axis=0)
+            proj_res_path.append(harm_proj_path)
+            proj_res_val.append(harm_proj_val)
+
+        plt.figure()
+        SC.plot_trajectories(trajectory.path_edges)
+
+    for val in proj_res_val:
+        print(val)    
+    plt.show()
+
+
+
+    """all_faces = np.arange(S.face_vec.shape[0])
+    faces_to_add = {2:set(all_faces)}
+    S.add_simplices(faces_to_add)
+    S.make_holes(hole_locs, r)
+    
+    nodes_to_cross = coord_to_node(S, [(-1,-1), (0,0), (1,1)], 3)
+    print(nodes_to_cross)
+
+    start, end, num_trajectories = 2 * (n_side + 1), (n_side-1) ** 2 - 3, 2
+    paths = generate_paths(S, start, end, num_trajectories)
+
+    back_path = [(b, a) for (a,b) in paths[1][::-1]]
+    new_path = path_interp(S, paths[0], back_path)
+
+    for path in new_path:
+        x_vec = np.zeros_like(S.edge_vec)
+        parity_sum = 0
+        for i, edge in enumerate(path):
+            e1, e2 = edge
+            parity = -2 * int(e1 > e2) + 1
+            actual_edge = ( min([e1, e2]), max([e1, e2]) )
+            edge_idx = S.edges.index(actual_edge)
+            x_vec[edge_idx] += parity
+        print(np.sum(S.harm_proj(x_vec), axis=0))
+
+    S.plot_trajectories(paths)
+    plt.show()"""
+
+
